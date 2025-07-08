01a4153bdd2ef2f1b2045fb5db79ba7ba75cf6d0
shaharyar20 <103658450+shaharyar20@users.noreply.github.com>
2025-07-08 17:42:39 +0200

    Delete softwarelabgrp15-main directory

--- softwarelabgrp15-main/laserOptimizer/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py
-from .lasers import *
-from .lossFunctional import *
-from .helpers import *
-from .ForwardSolver import ForwardSolver
-from .InverseSolver import InverseSolver
-from .ProblemSettings import *
